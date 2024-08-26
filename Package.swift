4c79444e1f65fd847efc94d57f9615773bc73b28
ras0q <ras0q@users.noreply.github.com>
2024-08-26 03:39:47 +0000

    Add SwiftWasm Client

--- Package.swift
+++ Package.swift
@@ -7,12 +7,16 @@ let package = Package(
     name: "App",
     dependencies: [
         .package(url: "https://github.com/hummingbird-project/hummingbird", from: "2.0.0-rc.4"),
-        .package(url: "https://github.com/vapor/mysql-kit.git", from: "4.0.0")
+        .package(url: "https://github.com/TokamakUI/Tokamak", from: "0.11.0"),
+        .package(url: "https://github.com/vapor/mysql-kit.git", from: "4.0.0"),
     ],
     targets: [
         .executableTarget(name: "App", dependencies: [
             .product(name: "Hummingbird", package: "hummingbird"),
             .product(name: "MySQLKit", package: "mysql-kit")
+        ]),
+        .executableTarget(name: "Client", dependencies: [
+            .product(name: "TokamakShim", package: "Tokamak")
         ])
     ]
 )
---
+++ Sources/Client/main.swift
@@ -0,0 +1,16 @@
+import TokamakShim
+
+@main
+struct TokamakApp: App {
+    var body: some Scene {
+        WindowGroup("Tokamak App") {
+            ContentView()
+        }
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+    }
+}
